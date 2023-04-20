bafa6f769b2ea4349c8bd208401cf24fce1e1dd8
Jack Roudebush <roudebushjack@gmail.com>
2023-04-20 00:10:48 -0400

    Complete substrings exercise

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-def substrings(word, dictionary)
 
+def substrings(word, dictionary)
+    match_hash = {}
+    dictionary.each do |dict_item|
+        if word.downcase.include?(dict_item)
+            match_hash[dict_item] = word.downcase.scan(/#{dict_item}/).size
+        end
+    end
+    match_hash
 end
 
-substrings("below",dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
